605c618c5abb63b4fb634964eaaf2e715a2e3fbf
hongxin <hx754835899@gmail.com>
2019-02-27 01:53:24 +0800

    props, state 与render函数之间的关系

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,12 @@
 import React, {Component, Fragment} from 'react';
 import TodoItem from './TodoItem';
+import Test from './Test'
 import './index.css'
 
 class TodoList extends Component{
     constructor(props) {
         super(props);
+        // 当组件的state或者props发生改变的时候, render函数就会执行
         this.state = {
             inputValue: '',
             list: []
@@ -32,6 +34,7 @@ class TodoList extends Component{
                 <ul>
                   {this.getTodoItem()}
                 </ul>
+              <Test content={this.state.inputValue}/>
             </Fragment>
             )
     }
